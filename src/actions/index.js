0bbb80ccc1f097aae25c900fb6a999a58c68de09
Augusto Sandim <augusto@jera.com.br>
2018-05-05 13:07:49 -0300

    Add delete reminder

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,19 @@
-import { ADD_REMINDER } from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
 
 export const addReminder = text => {
   const action = {
     type: ADD_REMINDER,
     text
   }
-
   console.log('action in addReminder', action);
+  return action;
+}
 
+export const deleteReminder = id => {
+  const action = {
+    type: DELETE_REMINDER,
+    id
+  }
+  console.log('deleting in actions', action)
   return action;
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addReminder } from '../actions';
+import { addReminder, deleteReminder } from '../actions';
 
 class App extends Component {
   constructor(props) {
@@ -15,6 +15,10 @@ class App extends Component {
     this.props.addReminder(this.state.text);
   }
 
+  deleteReminder(id) {
+    this.props.deleteReminder(id)
+  }
+
   renderReminders() {
     const { reminders } = this.props;
 
@@ -24,7 +28,13 @@ class App extends Component {
           reminders.map(reminder => {
             return (
               <li key={ reminder.id } className="list-group-item">
-                <div>{reminder.text}</div>
+                <div className="list-item">{reminder.text}</div>
+                <div
+                  className="list-item delete-button"
+                  onClick={() => this.deleteReminder(reminder.id)}
+                >
+                  &#x2715;
+                </div>
               </li>
             )
           })
@@ -75,4 +85,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, { addReminder })(App);
+export default connect(mapStateToProps, { addReminder, deleteReminder })(App);
